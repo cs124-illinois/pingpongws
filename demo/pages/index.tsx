e4a85444443dfe0aa32d600aa925016b0c9227eb
Geoffrey Challen <challen@illinois.edu>
2023-01-12 12:42:10 -0600

    Updates.

--- demo/pages/index.tsx
+++ demo/pages/index.tsx
@@ -2,33 +2,12 @@ import { filterPingPongMessages, PingWS } from "@cs124/pingpongws-client"
 import { useEffect, useRef, useState } from "react"
 import ReconnectingWebSocket from "reconnecting-websocket"
 
-const safeClose = (ws: ReconnectingWebSocket | undefined) => {
-  if (!ws) {
-    return
-  }
-  try {
-    if (ws.readyState === ws.CLOSED || ws.readyState === ws.CLOSING) {
-      return
-    }
-    if (ws.readyState === ws.OPEN) {
-      ws.close()
-    } else {
-      ws.addEventListener("open", () => {
-        if (ws.readyState !== ws.CLOSED && ws.readyState !== ws.CLOSING) {
-          ws.close()
-        }
-      })
-    }
-  } catch (err) {
-    console.log(err)
-  }
-}
 const PingPongWSDemo: React.FC = () => {
   const [connected, setConnected] = useState(false)
   const connection = useRef<ReconnectingWebSocket | undefined>()
 
   useEffect(() => {
-    safeClose(connection.current)
+    connection.current?.close()
     connection.current = PingWS(new ReconnectingWebSocket(process.env.NEXT_PUBLIC_DEMO_SERVER as string), {
       verbose: true,
     })
@@ -46,7 +25,7 @@ const PingPongWSDemo: React.FC = () => {
       })
     )
     return () => {
-      safeClose(connection.current)
+      connection.current?.close()
     }
   }, [])
 
